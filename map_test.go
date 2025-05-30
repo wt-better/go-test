160b9539bee4656dddbe470b1bb9b778a1b00854
yixia.wt <yixia.wt@alibaba-inc.com>
2025-05-30 15:48:15 +0800

    add test

---
+++ map_test.go
@@ -0,0 +1,17 @@
+package go_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Manager struct {
+	appName string
+}
+
+func Test1(t *testing.T) {
+	//var mnr = map[string]Manager{}
+	//s := mnr[""]
+	//println("s:" + s.appName)
+	print(fmt.Sprintf("feed %v is not ck transport", "123"))
+}
